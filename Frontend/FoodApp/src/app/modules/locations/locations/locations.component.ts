415704f5e614fa59500ff3dff8daf2faba9c2c46
Robertto <73616883+crime-story@users.noreply.github.com>
2022-08-31 05:48:16 +0300

    created interaction between backend and frontend

--- Frontend/FoodApp/src/app/modules/locations/locations/locations.component.ts
+++ Frontend/FoodApp/src/app/modules/locations/locations/locations.component.ts
@@ -2,6 +2,7 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { DataService } from 'src/app/services/data.service';
+import { LocationsService } from 'src/app/services/locations.service';
 
 @Component({
   selector: 'app-bands',
@@ -12,11 +13,25 @@ export class LocationsComponent implements OnInit, OnDestroy {
   public subscription!: Subscription;
   public loggedUser!: { username: string; password: string; };
   public parentMessage = 'message from parent';
-  constructor(private router: Router, private dataService: DataService) {}
+  public locations = [];
+  public displayedColumns = ['id', 'country', 'city', 'street'];
+
+  constructor(
+    private router: Router, 
+    private dataService: DataService,
+    private locationsService: LocationsService
+    ) {}
 
   ngOnInit() {
     this.subscription = this.dataService.currentUser.subscribe(
       (user) => (this.loggedUser = user)
+    );
+    this.locationsService.getAll().subscribe(
+      (result) => {
+        console.log(result);
+        this.locations = result;
+      },
+      (error) => console.error(error)
     );
   }
   ngOnDestroy() {
---
+++ Frontend/FoodApp/src/app/services/locations.service.ts
@@ -0,0 +1,18 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LocationsService {
+  public url = 'https://localhost:7225/api/location';
+  constructor(
+    public http: HttpClient,
+
+  ) { }
+
+  public getAll(): Observable<any> {
+    return this.http.get(`${this.url}/Get-select`);
+  }
+}
